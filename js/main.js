fbd9767836bba73b1781a8bc74bd3d5b82bc7d47
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2024-08-28 15:49:16 -0300

    Se agregan el archivo js y json

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//ALERT: salida de datos
+alert("Bienvenidos");
+
+//PROMPT: entrada de datos
+let nombreIngresado = prompt("Ingrese su nombre");
+console.log(nombreIngresado);
+
+//Template literals ´${}´
