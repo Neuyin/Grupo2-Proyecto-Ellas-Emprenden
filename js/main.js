59f0e2e96ad6febd79e41fb9b3c0a527417bb84b
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2024-08-28 16:41:52 -0300

    Se agrega alert para registro exitoso en la plataforma

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-//ALERT solo de home
 window.onload = function() {
+    // ALERT solo de home
     if (window.location.pathname.includes("index.html")) {
         alert("¡Bienvenid@ a nuestra plataforma de emprendedoras!");
     }
-};
 
-//Se agrega PROMPT en el formulario de inicio de sesión
-document.getElementById("loginForm").onsubmit = function() {
-    let username = prompt("Ingresa tu nombre de usuario:");
-    let additionalInfo = prompt("Hola " + username + ", ¿Cuál es el principal desafío que has enfrentado en tu emprendimiento?");
-    console.log("Usuario: " + username + ", Desafío: " + additionalInfo);
-    return true;
-};
+    // Se agrega PROMPT en el formulario de inicio de sesión
+    let loginForm = document.getElementById("loginForm");
+    if (loginForm) {
+        loginForm.onsubmit = function() {
+            let username = prompt("¡Volviste! ¿Nos ayudas con una encuesta? Queremos mejorar nuestro servicio. Por favor ingresá tu nombre de usuario:");
+            let additionalInfo = prompt("Hola " + username + ", ¿Cuál es el principal desafío que has enfrentado en tu emprendimiento? Gracias por responder.");
+            console.log("Usuario: " + username + ", Desafío: " + additionalInfo);
+            return true;
+        };
+    }
 
+    // Alert para registro exitoso en la plataforma
+    let registerForm = document.getElementById("registerForm");
+    if (registerForm) {
+        registerForm.onsubmit = function() {
+            console.log("Formulario de registro enviado."); //Para verificar funcionamiento
+            alert("Registro exitoso. ¡Bienvenida a la plataforma!");
+            return true;
+        };
+    }
+};
 
 
 //PROMPT: entrada de datos
