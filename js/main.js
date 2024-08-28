1bd688a61bf94c39652e6377ab981ae378dd4c3d
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2024-08-28 16:27:22 -0300

    Se agrega alert para home y prompt para inicio de sesión

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
+//ALERT solo de home
+window.onload = function() {
+    if (window.location.pathname.includes("index.html")) {
+        alert("¡Bienvenid@ a nuestra plataforma de emprendedoras!");
+    }
+};
 
-//ALERT: salida de datos
-alert("Bienvenidos");
+//Se agrega PROMPT en el formulario de inicio de sesión
+document.getElementById("loginForm").onsubmit = function() {
+    let username = prompt("Ingresa tu nombre de usuario:");
+    let additionalInfo = prompt("Hola " + username + ", ¿Cuál es el principal desafío que has enfrentado en tu emprendimiento?");
+    console.log("Usuario: " + username + ", Desafío: " + additionalInfo);
+    return true;
+};
+
+
 
 //PROMPT: entrada de datos
-let nombreIngresado = prompt("Ingrese su nombre");
-console.log(nombreIngresado);
+//let nombreIngresado = prompt("Ingrese su nombre");
+//console.log(nombreIngresado);
 
 //Template literals ´${}´
